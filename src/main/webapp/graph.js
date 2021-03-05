407a770d741bec2448dd1711fd201fa996faabde
Weiting Cai <w.cai@student.utwente.nl>
2021-03-05 12:37:15 +0100

    fix clipboard

--- src/main/webapp/graph.js
+++ src/main/webapp/graph.js
@@ -2,12 +2,6 @@
 let cy = cytoscape({
     container: document.getElementById("cy"),
     style: [
-        // {
-        //     selector: '#node0',
-        //     style: {
-        //         'label': "1"
-        //     }
-        // },
         {
             selector: 'node[name]',
             style: {
