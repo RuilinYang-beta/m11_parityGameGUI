7e18dafb409bfdc4c9af860a5acf3f65b63f42b0
Weiting Cai <w.cai@student.utwente.nl>
2021-03-05 12:27:20 +0100

    clipboard

--- src/main/webapp/undo_redo.js
+++ src/main/webapp/undo_redo.js
@@ -26,6 +26,15 @@ document.addEventListener("keydown", function (e) {
             ur.undo();
         else if (e.which === 89)
             ur.redo();
+        else if (e.which == 67) // CTRL + C
+            cy.clipboard().copy(cy.$(":selected"));
+        else if (e.which == 86) // CTRL + V
+            ur.do("paste");
+        else if (e.which == 65) {
+            cy.elements().select();
+            e.preventDefault();
+        }
+
 
 });
 
