ff1a089b81c31d131f515bd4d6cf3a41156224e2
Weiting Cai <w.cai@student.utwente.nl>
2021-04-09 17:06:51 +0200

    merge

--- target/GameGUI_v1/layout.js
+++
@@ -1,9 +0,0 @@
-
-cy.on("drag", function(evt) {
-    if (auto_organizing)
-        cy.layout({name: 'cola', colasetting}).run();
-});
-cy.on("dragfree", function(evt) {
-    if (auto_organizing)
-        cy.layout({name: 'cola', colasetting}).run();
-});
--- target/GameGUI_v1/modal_effect.js
+++
@@ -1,9 +0,0 @@
-function toggle_arrow(elem) {
-
-    if (elem.getAttribute("aria-expanded") == "false") {
-        elem.getElementsByTagName("i")[0].setAttribute("class", "arrow down");
-    }
-    else {
-        elem.getElementsByTagName("i")[0].setAttribute("class", "arrow right");
-    }
-}
